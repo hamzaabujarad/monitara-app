b381c8a5d46e9b57a2cdc0ac4cfef051173085b7
hamza ahmad <hamzaahmad@hamzas-iMac.local>
2022-01-14 12:22:21 +0200

    update mmkv storage correctly

--- android/app/src/main/java/com/monitara/CustomMMKVJSIModulePackage.java
+++ android/app/src/main/java/com/monitara/CustomMMKVJSIModulePackage.java
@@ -1,18 +1,23 @@
 package com.monitara;
+
 import com.facebook.react.bridge.JSIModuleSpec;
 import com.facebook.react.bridge.JavaScriptContextHolder;
 import com.facebook.react.bridge.ReactApplicationContext;
+
 import java.util.List;
 
-import com.swmansion.reanimated.ReanimatedJSIModulePackage; // <-- ADD THIS
-import com.ammarahmed.mmkv.RNMMKVModule; // <-- ADD THIS
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
+import com.ammarahmed.mmkv.RNMMKVModule;
 
-public class CustomMMKVJSIModulePackage extends ReanimatedJSIModulePackage { // Replace implements JSIModulePackage with extends ReanimatedJSIModulePackage
+
+public class CustomMMKVJSIModulePackage extends ReanimatedJSIModulePackage {
     @Override
     public List<JSIModuleSpec> getJSIModules(ReactApplicationContext reactApplicationContext, JavaScriptContextHolder jsContext) {
-        //super.getJSIModules(reactApplicationContext, jsContext); // <-- ADD THIS
-        reactApplicationContext.getNativeModule(RNMMKVModule.class).installLib(jsContext, reactApplicationContext.getFilesDir().getAbsolutePath() + "/mmkv");
+        reactApplicationContext.getNativeModule(RNMMKVModule.class).installLib(
+            jsContext,
+            reactApplicationContext.getFilesDir().getAbsolutePath() + "/mmkv"
+        );
 
-        return super.getJSIModules(reactApplicationContext, jsContext); // <-- ADD THIS
+        return super.getJSIModules(reactApplicationContext, jsContext);
     }
 }
